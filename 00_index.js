b7d8bda5205b719bf395f52b5d3cdec9f1c72264
BryanAdamss <bryanadamss@foxmail.com>
2023-01-17 11:49:24 +0800

    chore: first version

---
+++ 00_index.js
@@ -0,0 +1,15 @@
+/**
+ * @author GuangHui
+ * @description 入口
+ */
+
+let price = 10
+let quantity = 1
+let total = price * quantity
+
+console.log(`total is ${total}`)
+
+quantity = 2
+
+// total 未自动重新计算
+console.log(`total is ${total}`)
